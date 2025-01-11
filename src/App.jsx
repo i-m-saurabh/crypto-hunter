3c8c437884dc6ad6852e04a5728feffc9b0810ca
Saurabh <ksaurabh2005@gmail.com>
2025-01-12 00:07:38 +0530

    Header completed and currency selection using ContextAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './Components/Header'
+import Homepage from './Pages/Homepage'
+import Coinpage from './Pages/Coinpage'
+import { makeStyles, styled } from '@mui/material'
 
 function App() {
 
+  const AppContainer = styled('div')(({ theme }) => ({
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight: '100vh',
+  }));
+
   return (
     <BrowserRouter>
-      <div>
+      <AppContainer>
         <Header />
-      </div>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/coins/:id' element={<Coinpage />} />
+        </Routes>
+      </AppContainer>
     </ BrowserRouter>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,61 @@
+import { AppBar, Container, createTheme, MenuItem, Select, styled, ThemeProvider, Toolbar, Typography } from '@mui/material'
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import { CryptoState } from '../CryptoContext';
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const {currency, setCurrency} = CryptoState();    
+
+    const CryptoHunterContainer = styled(Typography, {
+        shouldForwardProp: (prop) => true,
+      })(({ theme }) => ({
+        flex: 1,
+        color: 'gold',
+        fontFamily: 'Montserrat',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+      }));
+    
+      const darkTheme = createTheme({
+      palette: {
+        primary:{
+          main: "#fff",
+        },
+        mode: "dark",
+      }
+    })
+    
+
     return(
-        <div>
-            Header
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color='transparent' position='static'>
+                <Container>
+                    <Toolbar>
+                        <CryptoHunterContainer 
+                            onClick={()=> navigate('/')}
+                            variant='h5' 
+                        >
+                            Crypto Hunter
+                        </CryptoHunterContainer>
+                        <Select 
+                            variant='outlined' 
+                            style={{
+                                width: 100,
+                                height: 40,
+                                marginRight:15,
+                            }}
+                            value={currency}
+                            onChange={(e)=>{setCurrency(e.target.value)}}
+                        >
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"INR"}>INR</MenuItem>
+                        </Select>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
     )
 }
 
